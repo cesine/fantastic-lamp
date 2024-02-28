d3eeb4b924fa413da0d25530130bc8199586df8d
cesine <cesine@yahoo.com>
2024-02-28 17:10:08 -0600

    keep border on the top line

--- src/components/cryptogram/Cell.tsx
+++ src/components/cryptogram/Cell.tsx
@@ -44,15 +44,17 @@ export const Cell = ({
     : false
 
   const classesEncrypted = classnames(
-    'xxshort:w-11 xxshort:h-11 short:text-2xl short:w-12 short:h-12 w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white'
-  )
-
-  const classesDecrypted = classnames(
     'xxshort:w-11 xxshort:h-11 short:text-2xl short:w-12 short:h-12 w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
       'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
         !status,
       'border-white dark:border-black': !status,
+    }
+  )
+
+  const classesDecrypted = classnames(
+    'xxshort:w-11 xxshort:h-11 short:text-2xl short:w-12 short:h-12 w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
+    {
       'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
         status === 'absent',
       'correct shadowed bg-orange-500 text-white border-orange-500':
