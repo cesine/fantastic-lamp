6d2e74c5365c421d27e4f6fc6229c7b9a07b5b03
cesine <cesine@yahoo.com>
2024-02-28 17:47:12 -0600

    remove more storybook scaffolding

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/preset-create-react-app'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {
