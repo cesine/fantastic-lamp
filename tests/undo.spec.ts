76029293f435d39e4582b2a199f3a27c736a2994
cesine <cesine@yahoo.com>
2024-02-29 14:39:02 -0600

    seperate user stories

--- tests/undo.spec.ts
+++ tests/undo.spec.ts
@@ -6,7 +6,7 @@ test.describe('undo tests', () => {
   test.beforeEach(({ page }, testInfo) => {
     collectConsole({ page }, testInfo)
   })
-  test('As a user I want to undo a guess by clicking on a cell', async ({
+  test('As a user I want to undo a guess by pushing backspace', async ({
     page,
   }) => {
     await page.goto('/')
@@ -32,13 +32,29 @@ test.describe('undo tests', () => {
     expect(page.getByRole('button', { name: 'C' })).toHaveText('E')
     await page.keyboard.press('Backspace')
     expect(page.getByRole('button', { name: 'C' })).toHaveText('')
+  })
+
+  test('As a user I want to undo a guess by clicking on a cell', async ({
+    page,
+  }) => {
+    await page.goto('/')
+    await page.getByLabel('How to play').getByRole('button').click()
+
+    await page.getByRole('button', { name: 'A' }).click()
+    await page.keyboard.type('t')
+    expect(page.getByRole('button', { name: 'A' })).toHaveText('T')
 
     await page.getByRole('button', { name: 'B' }).click()
+    await page.keyboard.type('h')
+    expect(page.getByRole('button', { name: 'B' })).toHaveText('H')
+
+    await page.getByRole('button', { name: 'C' }).click()
+    await page.keyboard.type('E')
+
     await page.getByRole('button', { name: 'B' }).click()
     await page.keyboard.press('Backspace')
     expect(page.getByRole('button', { name: 'B' })).toHaveText('')
 
-    await page.getByRole('button', { name: 'A' }).click()
     await page.getByRole('button', { name: 'A' }).click()
     await page.keyboard.press('Backspace')
     expect(page.getByRole('button', { name: 'A' })).toHaveText('')
