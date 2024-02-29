531576ae0813090e1514c0837105f7610ec47382
cesine <cesine@yahoo.com>
2024-02-28 18:07:21 -0600

    workaround  Cannot find module './node_modules/@storybook/react-webpack5/dist/package.json' Require stack: - /Users/username/.npm/_npx/fb679deafd0f9e87/node_modules/@storybook/telemetry/dist/index.js

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,24 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import path from 'path'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(mjs|ts|tsx)'],
   addons: ['@storybook/preset-create-react-app'],
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
   framework: {
-    name: '@storybook/react-webpack5',
+    name: path.resolve(
+      require.resolve('@storybook/react-webpack5/preset'),
+      '..'
+    ) as any,
     options: {
       builder: {
         useSWC: true,
       },
     },
   },
+
   docs: {
     autodocs: 'tag',
   },
