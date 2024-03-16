54a608afa0f9761c0d7a179f881fa387e870cc95
cesine <cesine@yahoo.com>
2024-03-16 12:51:36 -0400

    Closed beta test (#119)
    * disable navbar icons if not clickable

---
+++ tests/closed-beta.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('cryptogram tests', () => {
+  test('should show a link for how to join the beta', async ({
+    page,
+    isMobile,
+  }) => {
+    await page.goto('/')
+
+    expect(await page.getByText('Please use the link provided')).toHaveText(
+      'Please use the link provided to you, or click the link below to get beta access.Get my link!'
+    )
+
+    expect(
+      await page.getByRole('link', { name: 'Get my link!' })
+    ).toHaveAttribute('href', 'https://forms.gle/FRjSYoG2Js6Pv8QD9')
+
+    // Mobile devices will use an intent instead of a link, so only test the link on desktop
+    // Google doesnt like it if you open forms in an automated test
+  })
+})
