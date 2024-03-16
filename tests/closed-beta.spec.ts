12a6251731ab0f9136c986e942d0c33a832bacb0
cesine <cesine@yahoo.com>
2024-03-16 11:31:29 -0400

    update test to handle mobile intent

--- tests/closed-beta.spec.ts
+++ tests/closed-beta.spec.ts
@@ -1,16 +1,23 @@
 import { expect, test } from '@playwright/test'
 
 test.describe('cryptogram tests', () => {
-  test('should show a link for how to join the beta', async ({ page }) => {
-    await page.goto('http://localhost:3000/')
+  test('should show a link for how to join the beta', async ({
+    page,
+    isMobile,
+  }) => {
+    await page.goto('/')
 
     expect(await page.getByText('Please use the link provided')).toHaveText(
       'Please use the link provided to you, or click the link below to get beta access.Get my link!'
     )
 
     await page.getByRole('link', { name: 'Get my link!' }).click()
-    expect(
-      await page.getByRole('heading', { name: 'Sign in to continue' })
-    ).toHaveText('Sign in to continue')
+
+    // Mobile devices will use an intent instead of a link, so only test the link on desktop
+    if (!isMobile) {
+      expect(
+        await page.getByRole('heading', { name: 'Sign in to continue' })
+      ).toHaveText('Sign in to continue')
+    }
   })
 })
