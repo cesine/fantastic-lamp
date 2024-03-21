573eb5eeb361baa7b0c4889e9022aff7ac6eb01c
cesine <cesine@yahoo.com>
2024-03-20 21:40:35 -0400

    update book link expectation

--- tests/book.link.spec.ts
+++ tests/book.link.spec.ts
@@ -6,8 +6,7 @@ test.describe('cryptogram tests', () => {
     await page.locator('.absolute').click()
     await page.getByLabel('Open Heart Modal').click()
     await page.getByRole('link', { name: 'Book cover' }).click()
-    expect(await page.url()).toEqual('hi')
-    await page.goto(
+    expect(await page.url()).toEqual(
       'https://www.amazon.com/dp/B0CKKF3T3Z?ref_=cm_sw_r_cp_ud_dp_ZMQ0AYWGKSXTGP5S3JQ4'
     )
   })
