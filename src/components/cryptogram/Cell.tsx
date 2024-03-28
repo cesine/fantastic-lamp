5a626b6685accd193defa818f96b7add69255a4a
cesine <cesine@yahoo.com>
2024-03-27 22:25:50 -0400

    make colors consistant

--- src/components/cryptogram/Cell.tsx
+++ src/components/cryptogram/Cell.tsx
@@ -52,8 +52,9 @@ export const Cell = ({
   const classesDecrypted = classnames(
     'xxshort:w-4 xxshort:h-4 short:text-2xl short:w-6 short:h-6 w-8 h-8 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-thin rounded dark:text-white',
     {
-      'hover:bg-slate-300 active:bg-slate-400 dark:border-slate-700': !status,
-      'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
+      'hover:bg-federal-blue-300 active:bg-federal-blue-400 dark:border-federal-blue-700':
+        !status,
+      'absent shadowed text-white border-federal-blue-400 dark:border-federal-blue-700':
         status === 'absent',
       'correct shadowed bg-cyan-500 hover:bg-orange-600 active:bg-orange-700 text-white border-cyan-500':
         status === 'correct' && isHighContrast,
@@ -61,7 +62,7 @@ export const Cell = ({
         status === 'present' && isHighContrast,
       'correct shadowed bg-green-700 hover:bg-green-600 active:bg-green-700 text-white border-green-700':
         status === 'correct' && !isHighContrast,
-      'present shadowed bg-dark-goldenrod hover:bg-yellow-600 active:bg-yellow-700 text-white border-dark-goldenrod':
+      'present shadowed bg-dark-goldenrod hover:bg-yellow-500 active:bg-yellow-700 text-white border-dark-goldenrod':
         status === 'present' && !isHighContrast,
       'cell-fill-animation': isFilled,
       'cell-reveal': shouldReveal,
