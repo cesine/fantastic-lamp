f2b31900832957842086597269981e3cdd979f14
cesine <cesine@yahoo.com>
2024-03-27 21:19:09 -0400

    add class for hr in nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,13 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
-    
-
     extend: {
-      
       colors: {
         'dark-spring-green': '#1E6E26',
-        "dark-goldenrod": '#B58B2A',
+        'dark-goldenrod': '#B58B2A',
         'federal-blue': '#1e293b',
-        'claret': '#832232',
+        claret: '#832232',
       },
-
 
       screens: {
         short: { raw: '(max-height: 650px)' },
